f9edac01f4933d8cfee8ec89046ccc267c91fc09
Lina0596 <lina.dahlhaus@icloud.com>
2025-08-11 10:14:12 +0200

    add links to social profiles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
   return (
-    <div className="flex items-center justify-center h-16 bg-black text-white font-medium">
+    <div className="flex items-center justify-between h-16 bg-black text-white font-medium px-4 xl:px-45 lg:px-40 md:px-20 sm:px-10">
       <p>© 2025 pawliday</p>
+      <div className="flex gap-4">
+        <p>Lina Dahlhaus</p>
+        <p>|</p>
+        <Link to="https://www.linkedin.com/in/lina-dahlhaus/" target="_blank">
+          LinkedIn
+        </Link>
+        <Link to="https://github.com/Lina0596" target="_blank">
+          GitHub
+        </Link>
+      </div>
     </div>
   );
 }
